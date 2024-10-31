5cbdc4e083e67454de8cfffb88ba46d9707c6a8c
longho <longhaiho696@gmail.com>
2024-11-01 00:20:55 +1100

    Remove client

--- client/src/app/page.js
+++
@@ -1,9 +0,0 @@
-import { redirect } from 'next/navigation';
-
-
-export default function Page() {
-  // Redirect to "/dashboard" immediately when this page is rendered
-    redirect('/pages/home');
-
-    return {}; // No UI needed since it redirects
-}
